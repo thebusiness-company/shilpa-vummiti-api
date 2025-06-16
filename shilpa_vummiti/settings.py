7aba21d9f481600ee3c2dacea0a49d7d65a5835e
thebusiness-company <thebusinesscomponyo12@gmail.com>
2025-06-16 12:50:32 +0530

    settings file change for database

--- shilpa_vummiti/settings.py
+++ shilpa_vummiti/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'shilpa_vummiti.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(os.getenv('DATABASE_URL'))
 }
 
 # DATABASES = {
