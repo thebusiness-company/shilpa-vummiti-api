39cf031a31563120682b387e953fc3bd1c12f4be
thebusiness-company <thebusinesscompany012@gmail.com>
2025-07-29 12:24:51 +0530

    Update settings.py

--- shilpa_vummiti/settings.py
+++ shilpa_vummiti/settings.py
@@ -111,20 +111,20 @@ WSGI_APPLICATION = 'shilpa_vummiti.wsgi.application'
 import dj_database_url
 import os
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
